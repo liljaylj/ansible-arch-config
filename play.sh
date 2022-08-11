4cdce655b05fdecb94ea6f2e612ebad7c17b0040
Zhanibek Adilbekov <zhanibek.adilbekov@proton.me>
2022-08-11 07:43:51 +0600

    feat(play.sh): add option to load vault password from file

--- play.sh
+++ play.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
+set -Eeuo pipefail
+
 # install prerequisites
 ansible-galaxy collection install -r requirements.yml
 
 # play
-ansible-playbook -i hosts.yml config.yml -e@.vars.yml --ask-vault-pass
+case "$1" in
+    ask)
+        vault_args=('--ask-vault-pass')
+    ;;
+    *)
+        vault_args=('--vault-pass-file' "$1")
+    ;;
+esac
+
+ansible-playbook -i hosts.yml config.yml -e@.vars.yml "${vault_args[@]}"
