aff5265939e55f3af33809430526364b782afb07
Zhanibek Adilbekov <zhanibek.adilbekov@proton.me>
2022-08-22 22:40:52 +0600

    feat(play.sh): run system and flatpak update before play

--- play.sh
+++ play.sh
@@ -2,8 +2,31 @@
 
 set -Eeuo pipefail
 
-export ANSIBLE_NOCOWS=1
+: "${1:?provide argument to script - path to the file with vault password or \`ask\`}"
 
+read -rn1 -p 'Run system and flatpak update? [Y/n] ' yn
+
+if [[ -n "$yn" ]]; then
+    echo
+fi
+
+case "$yn" in
+[Yy] | '')
+    set -x
+    sudo paru -Syu
+    sudo flatpak update
+    set +x
+    ;;
+[Nn])
+    echo 'Skipping system and flatpak update...'
+    ;;
+*)
+    echo 'Invalid input. Was it mistyping?'
+    exit 1
+    ;;
+esac
+
+export ANSIBLE_NOCOWS=1
 
 # play
 case "$1" in
